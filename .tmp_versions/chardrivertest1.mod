8849ae4b60cab04a98436747f308644fccbdfa1a
mehamasum <mehamasum@gmail.com>
2016-11-24 00:16:48 +0600

    meha done too

--- .tmp_versions/chardrivertest1.mod
+++
@@ -1,2 +0,0 @@
-/home/mehamasum/Desktop/deviceDriver/chardrivertest1.ko
-/home/mehamasum/Desktop/deviceDriver/chardrivertest1.o
--- userapptest1.c
+++ userapptest1.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
 #include <fcntl.h>
 
-#define DEVICE "/dev/sysprogdevice"
+#define DEVICE "/dev/chardev1"
+#define DEVICE_write "/dev/chardev"
 
 int main(){
-	char ch;
+	char ch,dummy;
 	char read_buf[100], write_buf[100];
-
+	int temp;
 	int fd = open(DEVICE, O_RDWR);
 	if(fd == -1){
-		printf("file %s either does not exist or has been locked by others");
+		printf("file either does not exist or has been locked by others");
 		exit(-1);
 	}
-
+	int fd2 = open(DEVICE_write, O_RDWR);
 	printf("r = read from device, w= write to device, enter command: ");
 	scanf("%c" , &ch);
 	getchar();
@@ -22,10 +28,12 @@ int main(){
 		case 'w':
 		printf("enter data: ");
 		scanf("%[^\n]", write_buf);
-		write(fd, write_buf, sizeof(write_buf));
+		write(fd2, write_buf, sizeof(write_buf));
 		break;
 		case 'r':
-		read(fd, read_buf, sizeof(read_buf));
+		printf("How many chars to read?\n");
+		scanf("%d%c",&temp,&dummy);
+		read(fd, read_buf, temp);
 		printf("device: %s\n", read_buf);
 		break;
 
